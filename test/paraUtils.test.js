09e3b586d12672057d5ee7b4898320e305a8957f
MGApcDev <mgapcdev@gmail.com>
2017-02-12 00:53:17 +0100

    ping

--- test/paraUtils.test.js
+++ test/paraUtils.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 describe('Paragraph utilities:', function () {
-
     describe('Get words of paragraph:', function () {
         it('Plain words', function () {
             expect(0).toEqual(1);
