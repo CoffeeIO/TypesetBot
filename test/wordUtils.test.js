09f06fa826000456a156e7d555549a52bf0a1638
MGApcDev <mgapcdev@gmail.com>
2017-02-24 21:08:09 +0100

    Add tests

--- test/wordUtils.test.js
+++ test/wordUtils.test.js
@@ -1,6 +1,12 @@
 'use strict';
 
 describe('Word utilities:', function () {
+    beforeEach(function(done) {
+        $('.unittest').remove(); // Remove existing tex documents
+        $('body').append(__html__['fixtures/test1']);
+        done();
+    });
+
     describe('Hyphenate word:', function () {
         it('Normal words', function () {
             var settings = TypesetBot.settings.validate();
@@ -17,6 +23,22 @@ describe('Word utilities:', function () {
         it('Language not found', function () {
             var settings = TypesetBot.settings.validate({hyphenLanguage: 'gg'});
             expect(TypesetBot.wordUtils.hyphenWord('hyphenate', settings)).toEqual(null);
+        });
+    });
+
+    describe('Word properties:', function () {
+        it('Normal words', function () {
+            var words = TypesetBot.paraUtils.getWords($('.plain').html());
+            expect(words.length).not.toBeLessThan(0)
+            var props = TypesetBot.wordUtils.getWordProperties($('.plain'), words);
+            expect(props.length).toEqual(words.length);
+            props.forEach(function (index, item) {
+                expect(item.width).not.toBeLessThan(0);
+                expect(item.str).toEqual(words[index]);
+            });
+        });
+        it('Empty array', function () {
+            expect(TypesetBot.wordUtils.getWordProperties($('.plain'), [])).toEqual([]);
         });
     });
 });
