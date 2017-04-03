674c28f74c594bc58d0f97da08c3a6fb88142e6f
MGApcDev <mgapcdev@gmail.com>
2017-04-03 11:44:58 +0200

    Add tests

--- test/nodeUtils.test.js
+++ test/nodeUtils.test.js
@@ -1,25 +1,14 @@
 'use strict';
 
 describe('Node utilities:', function () {
-    beforeEach(function(done) {
-        $('.unittest').remove(); // Remove existing tex documents
-        $('body').append(__html__['fixtures/test1']);
-        done();
+    describe('Check endtag:', function () {
+        it('Valid', function () {
+            expect(TypesetBot.nodeUtils.isEndTag('</b>')).toEqual(true);
+            expect(TypesetBot.nodeUtils.isEndTag('</span>')).toEqual(true);
+        });
+        it('Invalid', function () {
+            expect(TypesetBot.nodeUtils.isEndTag('<span>')).toEqual(false);
+            expect(TypesetBot.nodeUtils.isEndTag('<b class="test">')).toEqual(false);
+        });
     });
-
-    // describe('Node properties:', function () {
-    //     it('Normal words', function () {
-    //         var words = TypesetBot.paraUtils.getWords($('.plain').html());
-    //         expect(words.length).not.toBeLessThan(0);
-    //         var props = TypesetBot.nodeUtils.wordsToNodes($('.plain'), words);
-    //         expect(props.length).toEqual(words.length);
-    //         props.forEach(function (index, item) {
-    //             expect(item.width).not.toBeLessThan(0);
-    //             expect(item.str).toEqual(words[index]);
-    //         });
-    //     });
-    //     it('Empty array', function () {
-    //         expect(TypesetBot.nodeUtils.getWordProperties($('.plain'), [])).toEqual([]);
-    //     });
-    // });
 });
