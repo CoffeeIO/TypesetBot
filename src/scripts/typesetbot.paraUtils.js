cdc63c1157e124ca5c06caf8e80f5f36771ced52
Mathias Grundtvig Andreasen <mgapcdev@gmail.com>
2017-09-08 01:42:14 +0200

    Remove img and br tags and handle invisible elements (#50)

--- src/scripts/typesetbot.paraUtils.js
+++ src/scripts/typesetbot.paraUtils.js
@@ -4,7 +4,7 @@ TypesetBot.paraUtils = (function(obj) {
      * Get the width of a space with 'word-spacing: 0'.
      * Note: Width depends on font-family and font-size.
      */
-    obj.getDefaultSpaceWidth  = function (dom) {
+    obj.getDefaultSpaceWidth = function (dom) {
         dom.prepend('<span class="typeset-space">1<span> </span>1</span>');
         var elem = dom.find('.typeset-space');
         elem.css('word-spacing', '0');
@@ -16,7 +16,7 @@ TypesetBot.paraUtils = (function(obj) {
     /**
      * Set specific word spacing, adjusting for the font's default size of a space.
      */
-    obj.setSpaceWidth  = function (dom, value, unit) {
+    obj.setSpaceWidth = function (dom, value, unit) {
         var defaultWidth = obj.getDefaultSpaceWidth(dom);
         dom.css('word-spacing', 'calc(1' + unit + '*(' + value + ') - ' + defaultWidth + 'px)');
     };
@@ -24,9 +24,34 @@ TypesetBot.paraUtils = (function(obj) {
     /**
      * Return an array of words from some html text.
      */
-    obj.getWords  = function (html) {
+    obj.getWords = function (html) {
         return html.trim().replace( /\n/g, " " ).replace( /\s+/g, " " ).replace(/ (?![^<]*>)/g, '&nbsp;')
             .split('&nbsp;');
+    };
+
+    /**
+     * Remove breaks from paragraph.
+     */
+    obj.removeBreak = function (dom) {
+        var html = dom.html().replace(/<br[\/]?>/g, '');
+        dom.html(html);
+    };
+
+    /**
+     * Remove breaks from paragraph.
+     */
+    obj.removeImage = function (dom) {
+        console.warn('Found image inside paragraph');
+
+        var html = dom.html().replace(/<img[\/]?>/g, '');
+        dom.html(html);
+    };
+    /**
+     * Remove breaks from paragraph.
+     */
+
+    obj.isVisible = function (dom) {
+        return dom.is(':visible');
     };
 
     return obj;
