088826dab8d9e67e85adb503ab181c3d9dc95258
MGApcDev <mgapcdev@gmail.com>
2017-12-09 00:14:40 +0100

    Fix jquery build and module init

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('sass', function () {
 });
 
 var source = [
-    'src/scripts/vendor/jquery.min.js',
+    'src/scripts/vendor/typesetbotquery.js',
     'src/scripts/vendor/Queue.js',
     'src/scripts/preModule.js',
     'src/scripts/typesetbot.utils.js',
