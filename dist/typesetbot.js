13296e531b411f4210587c05b0d5263e162c6180
MGApcDev <mgapcdev@gmail.com>
2019-12-01 20:04:19 +0100

    Add comments for implementing linebreaking algorithm

--- dist/typesetbot.js
+++ dist/typesetbot.js
@@ -953,6 +953,76 @@ function TypesetBotTypeset(tsb) {
     // Tokenize nodes and store them.
     var tokens = this.tokenizer.tokenize(node);
     this.appendToTokenMap(node, tokens);
+    this._tbs.settings.loosenessParam = 0; // Check if node has changed content (inner nodes) since last typesetting.
+    // Make a copy of node which can be worked on without breaking webpage.
+    // Calculate linebreaks.
+    // func().
+    // Visually apply linebreaks to original element.
+    // Loop final solutions and find the one with lowest demerit.
+    // Construct lines.
+    // Word, Tag Space
+    // Close tags after each line.
+    // Start unfinished tags at the beginning of each line.
+    // Convert to HTML.
+  };
+
+  this.calcLinebreaks = function (node) {// Set space width based on settings.
+    // Init paragraph variables.
+    // Copy content.
+    // Get element width.
+    // Preprocess nodes.
+    // Tokenize.
+    // Get render sizes.
+    // Store these.
+    // Get font size of node.
+    // Calc space size.
+    // Calc space shrink.
+    // Calc space stretch.
+    // Queue of active breakpoints[]
+    // Shortest path {}
+    // FinalBreaks[]
+    // Counter for last rendered node.
+    // Preprocess all hyphens.
+    // Loop all nodes.
+    // Find hyphens in word.
+    // Get hyphen properties of that word.
+    // Create starting node.
+    // Loop until queue is empty.
+    // Init line variables.
+    // Check if shortest path is found.
+    // Get active node index.
+    // Get active hyphen index of node.
+    // Calculate adjustment ratio.
+    // Check if this is a valid breakpoint (not within min ratio).
+    // Loop hyphen breakpoints.
+    // Get hyphen adjustment ratio.
+    // If "valid" breaking point, generate break.
+    // Check if adjustment ratio is within the minimum ratio.
+    // Generate break.
+    // Add space width to current line with.
+    // Reset node content.
+    // Run linebreaking algorithm again if no solution was found.
+    // Increase looseness.
+    // Return calculated nodes and valid linebreak solutions.
+  };
+
+  this.addBreakpoint = function () {// Get fitness class
+    // Check flagDemerit
+    // Calc demerit
+    // Check fitness class change.
+    // Create breakpoint object.
+    // Update shorted path.
+  };
+
+  this.checkShortestPath = function () {
+    // Check if breakpoint if the lowest demerit on:
+    // "specific line, with specific nodex, with specific hyphen index".
+    return false;
+  };
+
+  this.updateShortestPath = function () {// If breakpoint has better demerit on "specific line, with specific nodex, with specific hyphen index".
+    // Then update the breakpoint.
+    // Append to active breakpoints queue.
   };
   /**
    * Add tokens to map for specific node.
