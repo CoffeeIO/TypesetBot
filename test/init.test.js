7045787f64ceb631a8f6c9af1ba521faa331aa4e
MGApcDev <mgapcdev@gmail.com>
2020-03-23 00:13:19 +0100

    Update test init

--- test/init.test.js
+++ test/init.test.js
@@ -1,10 +1,17 @@
 'use strict';
 
+afterEach(function() {
+    let x = document.body.querySelectorAll('.test');
+    for (let y of x) {
+        y.remove();
+    }
+});
+
 describe('Init TypesetBot:', function () {
     describe('Getting variable of instance', function () {
         it('New instance', function () {
             let tsb = new TypesetBot();
-            
+
             expect(tsb.logger).not.toEqual(undefined);
             expect(tsb.settings).not.toEqual(undefined);
         });
