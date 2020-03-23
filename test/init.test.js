a5e08a6657a869b15768926c246e6cd4dca0f77d
MGApcDev <mgapcdev@gmail.com>
2020-03-23 10:26:26 +0100

    Implement basic fixture and console log test

--- test/init.test.js
+++ test/init.test.js
@@ -7,13 +7,64 @@ afterEach(function() {
     }
 });
 
-describe('Init TypesetBot:', function () {
-    describe('Getting variable of instance', function () {
-        it('New instance', function () {
+describe('Setup', function () {
+
+    describe('Basic logic', function () {
+        function logMessage() {
+            console.log('Hello world');
+        }
+        function errrMessage() {
+            console.error('Hello world');
+        }
+
+        it('Basic math', function () {
+            let x = 1;
+            let y = 4;
+
+            expect(x + y).toEqual(5);
+        });
+        it('Class exists', function () {
             let tsb = new TypesetBot();
 
-            expect(tsb.logger).not.toEqual(undefined);
-            expect(tsb.settings).not.toEqual(undefined);
+            expect(TypesetBot).not.toEqual(null);
+            expect(TypesetBot).not.toEqual(undefined);
+        });
+        it('Console log spy', function () {
+            console.log = jasmine.createSpy("log");
+            console.error = jasmine.createSpy("error");
+
+            logMessage();
+            expect(console.log).not.toHaveBeenCalledWith('Hello worl');
+            expect(console.log).toHaveBeenCalledWith('Hello world');
+        });
+        it('Console error spy', function () {
+            console.error = jasmine.createSpy("error");
+            console.log = jasmine.createSpy("log");
+
+            errrMessage();
+
+            expect(console.log).not.toHaveBeenCalled();
+            expect(console.error).not.toHaveBeenCalledWith('Hello worl');
+            expect(console.error).toHaveBeenCalled();
+            expect(console.error).toHaveBeenCalledWith('Hello world');
+        });
+    });
+    describe('Inserting HTML', function () {
+        it('Adding fixture', function () {
+            let fixture =
+                '<div class="test">' +
+                    'Hello world' +
+                '</div>';
+
+            document.body.innerHTML = document.body.innerHTML + fixture;
+
+            let t = document.querySelector('.test');
+            expect(t.innerHTML).toEqual('Hello world');
+        });
+
+        it('Body is cleared after each test', function () {
+            let t = document.querySelector('.test');
+            expect(t).toEqual(null);
         });
     });
 });
