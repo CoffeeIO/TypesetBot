01a0be5322df93b45c54b2132c2879685ddfef21
MGApcDev <mgapcdev@gmail.com>
2020-03-28 10:33:08 +0100

    Implement basic test for render dimensions

--- test/init.test.js
+++ test/init.test.js
@@ -1,7 +1,17 @@
 'use strict';
 
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+beforeEach(function () {
+    // setTimeout(function () {
+    //     done();
+    // }, 50);
+});
+
 afterEach(function() {
-    let x = document.body.querySelectorAll('.test');
+    let x = document.body.querySelectorAll('.test, .style');
     for (let y of x) {
         y.remove();
     }
@@ -50,13 +60,13 @@ describe('Setup', function () {
         });
     });
     describe('Inserting HTML', function () {
-        it('Adding fixture', function () {
-            let fixture =
-                '<div class="test">' +
-                    'Hello world' +
-                '</div>';
+        let fixture =
+            '<div class="test">' +
+                'Hello world' +
+            '</div>';
 
-            document.body.innerHTML = document.body.innerHTML + fixture;
+        it('Adding fixture', function () {
+            document.body.insertAdjacentHTML('beforeend', fixture);
 
             let t = document.querySelector('.test');
             expect(t.innerHTML).toEqual('Hello world');
@@ -67,4 +77,66 @@ describe('Setup', function () {
             expect(t).toEqual(null);
         });
     });
+
+    describe('Render dimensions', function () {
+        let fixture =
+            '<div class="test">' +
+                'Hello world' +
+            '</div>';
+        it('Default style properties', function () {
+            document.body.insertAdjacentHTML('beforeend', fixture);
+
+            let t = document.querySelector('.test');
+
+            let fontSize = window.getComputedStyle(t).getPropertyValue('font-size');
+            expect(fontSize).not.toEqual(null);
+            expect(fontSize.indexOf('px')).not.toEqual(-1);
+        });
+        it('Overwrite style properties', function () {
+            let style =
+                '<style class="test">' +
+                    '.test { font-size: 29px; }' +
+                '<style>';
+
+            document.body.insertAdjacentHTML('beforeend', style);
+            document.body.insertAdjacentHTML('beforeend', fixture);
+
+            let t = document.querySelector('.test');
+
+            let fontSize = window.getComputedStyle(t).getPropertyValue('font-size');
+            expect(fontSize).toEqual('29px');
+        });
+        it('Removing style properties after test', function () {
+            document.body.insertAdjacentHTML('beforeend', fixture);
+
+            let t = document.querySelector('.test');
+
+            let fontSize = window.getComputedStyle(t).getPropertyValue('font-size');
+            expect(fontSize).not.toEqual('29px');
+        });
+        it('Getting width of container', function (done) {
+            let style =
+                '<style class="style">' +
+                    '.test { width: 500px; }' +
+                '<style>';
+
+            document.body.insertAdjacentHTML('beforeend', style);
+            document.body.insertAdjacentHTML('beforeend', fixture);
+
+            let t = document.querySelector('.test');
+            let widthProp = window.getComputedStyle(t).getPropertyValue('width');
+            expect(widthProp).toEqual('500px');
+
+            // Wait a bit for DOM to update.
+            setTimeout(function () {
+            // return timeout(3000).then(function(r) {
+                const rect = t.getBoundingClientRect();
+                const width = rect.right - rect.left;
+                expect(width).toEqual(500);
+                done();
+            }, 50);
+        });
+
+    });
 });
+
