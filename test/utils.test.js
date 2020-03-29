b57a46c6a53ee7cf3eb821ffbc49e90b7e12a93e
MGApcDev <mgapcdev@gmail.com>
2020-03-29 23:22:40 +0200

    Implement utils test

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,48 @@
 'use strict';
 
-describe('Testing utils.ts', function () {
+describe('utils.ts:', function () {
+    describe('createUUID', function() {
+        it('Uniqueness of uuid', function() {
+            let map = {};
+            for (let index = 0; index < 1000; index++) {
+                let uuid = TypesetBotUtils.createUUID();
+                map[uuid] = true;
+            }
 
+            expect(Object.keys(map).length).toEqual(1000);
+        });
+    });
+    describe('isVisible', function() {
+        it('Check display none', function() {
+            let fixture =
+                '<div class="test">' +
+                    'Hello world' +
+                '</div>';
+            let style =
+                '<style class="style">' +
+                    '.test { display: none; }' +
+                '<style>';
+
+            document.body.insertAdjacentHTML('beforeend', fixture);
+            expect(TypesetBotUtils.isVisible(document.querySelector('.test'))).toEqual(true);
+
+            document.body.insertAdjacentHTML('beforeend', style);
+            expect(TypesetBotUtils.isVisible(document.querySelector('.test'))).toEqual(false);
+        });
+        // it('Check empty element', function() {
+        //     let fixture =
+        //         '<div class="test">' +
+        //             '' +
+        //         '</div>';
+
+        //     document.body.insertAdjacentHTML('beforeend', fixture);
+        //     expect(TypesetBotUtils.isVisible(document.querySelector('.test'))).toEqual(false);
+        // });
+    });
+    describe('getArrayIndexes', function() {
+        it('General', function() {
+            expect(TypesetBotUtils.getArrayIndexes(["hyp", "hen", "ation"])).toEqual([3, 3]);
+            expect(TypesetBotUtils.getArrayIndexes(["hyptation"])).toEqual([]);
+        })
+    })
 });
