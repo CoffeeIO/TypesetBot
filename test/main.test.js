d52baaf903771278b0cf97304d86927cf0151889
MGApcDev <mgapcdev@gmail.com>
2020-04-10 13:05:59 +0200

    Implement height tests of TSB

--- test/main.test.js
+++ test/main.test.js
@@ -6,8 +6,139 @@ describe('main.ts:', function () {
             'Hello world' +
         '</div>';
 
+    let loremIpsum =
+        '<div class="test">' +
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis volutpat dolor non diam volutpat, laoreet porta risus sollicitudin. Phasellus cursus magna justo, a molestie metus aliquam a. Sed ut tellus non nunc iaculis pulvinar. Etiam suscipit nulla nec dui consectetur varius. Curabitur tortor odio, tincidunt at pharetra ut, varius a mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla elementum augue nulla, eget dictum odio auctor sit amet. Nunc commodo in eros at posuere. Aenean blandit auctor justo, porttitor laoreet arcu mattis eget. Nunc malesuada, enim vitae facilisis sagittis, nisl dolor fermentum eros, id euismod est sapien rhoncus velit. Curabitur id nisi id ligula convallis dapibus. Nam congue dolor eget cursus scelerisque. Etiam at sollicitudin nisl. Nullam semper mi dui, et ultrices arcu semper vitae. In quis metus felis.' +
+        '</div>';
+
+    let style =
+        '<style class="style">' +
+            '.test { width: 500px; line-height: 16px; }' +
+        '<style>';
+
+
     describe('constructor --', function() {
-        // @todo
+        it('Basic test', function (done) {
+
+            document.body.insertAdjacentHTML('beforeend', style);
+            document.body.insertAdjacentHTML('beforeend', loremIpsum);
+
+            let target = document.querySelector('.test');
+
+            expect(target.getBoundingClientRect().width).toEqual(500);
+            expect(document.querySelector('tsb-line')).toEqual(null);
+
+            let beforeHeight = target.getBoundingClientRect().height;
+
+            // Magic values.
+            expect(beforeHeight).toBeGreaterThan(100);
+            expect(beforeHeight).toBeLessThan(300);
+
+
+            let tsb = new TypesetBot('.test');
+
+            setTimeout(function() {
+
+                expect(document.querySelectorAll('tsb-line')).not.toEqual(null); // Typesetting worked.
+
+                expect(target.getBoundingClientRect().width).toEqual(500);
+
+                // Magic values.
+                expect(document.querySelectorAll('tsb-line').length).toBeGreaterThan(10);
+                expect(target.getBoundingClientRect().height).toBeGreaterThan(100);
+
+                // Height should be less than or equal.
+                expect(target.getBoundingClientRect().height).toBeLessThanOrEqual(beforeHeight);
+
+                done();
+            }, 100);
+        });
+
+        it('Mobile viewport', function (done) {
+            let style =
+                '<style class="style">' +
+                    '.test { width: 200px; line-height: 16px; }' +
+                '<style>';
+            let fontSize = 16;
+
+            document.body.insertAdjacentHTML('beforeend', style);
+            document.body.insertAdjacentHTML('beforeend', loremIpsum);
+
+            let target = document.querySelector('.test');
+
+            expect(target.getBoundingClientRect().width).toEqual(200);
+            expect(document.querySelector('tsb-line')).toEqual(null);
+
+            let beforeHeight = target.getBoundingClientRect().height;
+
+            // Magic values.
+            expect(beforeHeight).toBeGreaterThan(100);
+            expect(beforeHeight).toBeLessThan(600);
+
+
+            let tsb = new TypesetBot('.test');
+
+            setTimeout(function() {
+
+                expect(document.querySelectorAll('tsb-line')).not.toEqual(null); // Typesetting worked.
+
+                expect(target.getBoundingClientRect().width).toEqual(200);
+
+                // Magic values.
+                let lines = document.querySelectorAll('tsb-line').length;
+                expect(lines).toBeGreaterThan(10);
+
+                expect(target.getBoundingClientRect().height).toEqual(fontSize * lines);
+                // On small viewport this should produce smaller height.
+                expect(target.getBoundingClientRect().height).toBeLessThan(beforeHeight);
+
+                done();
+            }, 100);
+        });
+
+        it('Wide viewport', function (done) {
+            let style =
+                '<style class="style">' +
+                    '.test { width: 1000px; line-height: 16px; }' +
+                '<style>';
+            let fontSize = 16;
+
+            document.body.insertAdjacentHTML('beforeend', style);
+            document.body.insertAdjacentHTML('beforeend', loremIpsum);
+
+            let target = document.querySelector('.test');
+
+            expect(target.getBoundingClientRect().width).toEqual(1000);
+            expect(document.querySelector('tsb-line')).toEqual(null);
+
+            let beforeHeight = target.getBoundingClientRect().height;
+
+            // Magic values.
+            expect(beforeHeight).toBeGreaterThan(50);
+            expect(beforeHeight).toBeLessThan(200);
+
+
+            let tsb = new TypesetBot('.test');
+
+            setTimeout(function() {
+
+                expect(document.querySelectorAll('tsb-line')).not.toEqual(null); // Typesetting worked.
+
+                expect(target.getBoundingClientRect().width).toEqual(1000);
+
+                // Magic values.
+                let lines = document.querySelectorAll('tsb-line').length;
+                expect(lines).toBeGreaterThan(3);
+
+                expect(target.getBoundingClientRect().height).toEqual(fontSize * lines);
+                // Wide viewports should have the same height.
+                expect(target.getBoundingClientRect().height).toEqual(beforeHeight);
+
+                done();
+            }, 100);
+        });
+
+
     });
     describe('init --', function() { // constructor
         // @todo
@@ -35,6 +166,8 @@ describe('main.ts:', function () {
                 '</div>';
 
             document.body.insertAdjacentHTML('beforeend', fixture);
+
+            expect(document.querySelector('tsb-line')).toEqual(null);
 
             let tsb = new TypesetBot('.test');
 
