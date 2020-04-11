ade90ca2d555705b2f354d44d7c2ea85eb9d11ff
MGApcDev <mgapcdev@gmail.com>
2020-04-11 16:12:05 +0200

    Implement more shortest path tests

--- test/typeset.test.js
+++ test/typeset.test.js
@@ -29,10 +29,7 @@ describe('typeset.ts:', function () {
     describe('getBreakpoint --', function() {
         // @todo
     });
-    describe('isShortestPath --', function() { // Interesting
-        // @todo
-    });
-    describe('updateShortestPath --', function() { // Interesting
+    describe('updateShortestPath & isShortestPath --', function() { // Interesting
         it('Replacing solution based on demerit', function() {
 
             let typeset = new TypesetBotTypeset(new TypesetBot(null, { 'noRun': false }));
@@ -46,12 +43,85 @@ describe('typeset.ts:', function () {
             expect(typeset.shortestPath).toEqual({});
 
             expect(typeset.updateShortestPath(bp1)).toEqual(true);
-            expect(typeset.updateShortestPath(bp1)).toEqual(false);
-            expect(typeset.updateShortestPath(bp2)).toEqual(false);
-            expect(typeset.updateShortestPath(bp3)).toEqual(true);
+            expect(typeset.updateShortestPath(bp1)).toEqual(false); // Duplicate
+            expect(typeset.updateShortestPath(bp2)).toEqual(false); // Worse
+            expect(typeset.updateShortestPath(bp3)).toEqual(true);  // Better
 
             expect(typeset.isShortestPath(bp2)).toEqual(false);
             expect(typeset.isShortestPath(bp3)).toEqual(true);
+        });
+
+        it('Different lines', function() {
+
+            let typeset = new TypesetBotTypeset(new TypesetBot(null, { 'noRun': false }));
+
+            let bp1 = new TypesetBotLinebreak(null, 1, 0, 300, false, 0, 1, 16);
+            let bp2 = new TypesetBotLinebreak(null, 1, 0, 600, false, 0, 2, 16);
+            let bp3 = new TypesetBotLinebreak(null, 1, 0, 200, false, 0, 2, 16);
+            let bp4 = new TypesetBotLinebreak(null, 1, 0, 400, false, 0, 1, 16);
+
+
+            typeset.resetLineBreak();
+            expect(typeset.shortestPath).toEqual({});
+
+            expect(typeset.updateShortestPath(bp1)).toEqual(true);
+            expect(typeset.updateShortestPath(bp2)).toEqual(true);  // New line
+            expect(typeset.updateShortestPath(bp3)).toEqual(true);  // Better
+            expect(typeset.updateShortestPath(bp4)).toEqual(false); // Worse
+
+            expect(typeset.isShortestPath(bp1)).toEqual(true);
+            expect(typeset.isShortestPath(bp2)).toEqual(false);
+            expect(typeset.isShortestPath(bp3)).toEqual(true);
+            expect(typeset.isShortestPath(bp4)).toEqual(false);
+        });
+
+        it('Token index lines', function() {
+
+            let typeset = new TypesetBotTypeset(new TypesetBot(null, { 'noRun': false }));
+
+            let bp1 = new TypesetBotLinebreak(null, 9, 0, 300, false, 0, 3, 16);
+            let bp2 = new TypesetBotLinebreak(null, 10, 0, 200, false, 0, 3, 16);
+            let bp3 = new TypesetBotLinebreak(null, 9, 0, 200, false, 0, 3, 16);
+            let bp4 = new TypesetBotLinebreak(null, 10, 0, 400, false, 0, 3, 16);
+
+            typeset.resetLineBreak();
+            expect(typeset.shortestPath).toEqual({});
+
+            expect(typeset.updateShortestPath(bp1)).toEqual(true);
+            expect(typeset.updateShortestPath(bp2)).toEqual(true);  // New token
+            expect(typeset.updateShortestPath(bp3)).toEqual(true);  // Better
+            expect(typeset.updateShortestPath(bp4)).toEqual(false); // Worse
+
+            expect(typeset.isShortestPath(bp1)).toEqual(false);
+            expect(typeset.isShortestPath(bp2)).toEqual(true);
+            expect(typeset.isShortestPath(bp3)).toEqual(true);
+            expect(typeset.isShortestPath(bp4)).toEqual(false);
+        });
+
+        it('Hyphen index lines', function() {
+
+            let typeset = new TypesetBotTypeset(new TypesetBot(null, { 'noRun': false }));
+
+            let bp1 = new TypesetBotLinebreak(null, 9, null, 200, false, 0, 3, 16);
+            let bp2 = new TypesetBotLinebreak(null, 9, 1, 200, false, 0, 3, 16);
+            let bp3 = new TypesetBotLinebreak(null, 9, 0, 200, false, 0, 3, 16);
+            let bp4 = new TypesetBotLinebreak(null, 10, 0, 200, false, 0, 3, 16);
+            let bp5 = new TypesetBotLinebreak(null, 9, 0, 200, false, 0, 2, 16);
+
+            typeset.resetLineBreak();
+            expect(typeset.shortestPath).toEqual({});
+
+            expect(typeset.updateShortestPath(bp1)).toEqual(true);
+            expect(typeset.updateShortestPath(bp2)).toEqual(true);  // New hyphen
+            expect(typeset.updateShortestPath(bp3)).toEqual(true);  // New hyphen
+            expect(typeset.updateShortestPath(bp4)).toEqual(true);  // New token
+            expect(typeset.updateShortestPath(bp5)).toEqual(true);  // New line
+
+            expect(typeset.isShortestPath(bp1)).toEqual(true);
+            expect(typeset.isShortestPath(bp2)).toEqual(true);
+            expect(typeset.isShortestPath(bp3)).toEqual(true);
+            expect(typeset.isShortestPath(bp4)).toEqual(true);
+            expect(typeset.isShortestPath(bp5)).toEqual(true);
         });
     });
     describe('appendToTokenMap --', function() {
